10c8e5155f7578a379ec0f373e36be0d11bbd6c6
Gustavo Guerrero <gguerrerof98@gmail.com>
2021-12-29 16:33:52 -0500

    Did a tiny cleaned and organization of folders, also added styled-components for styles of components, and created a theme for the theme provider.

---
+++ src/lib/theme.js
@@ -0,0 +1,29 @@
+const theme = {
+  fonts: {
+    main: "'Outfit', sans-serif",
+  },
+  fontSize: {
+    paragraph:{
+      18: "1.125rem"
+    }
+  },
+  weights: {
+    300: 300,
+    400: 400,
+    600: 600,
+  },
+  colors: {
+    primary: {
+      softBlue: "hsl(215, 51%, 70%)",
+      cyan: "hsl(178, 100%, 50%)",
+    },
+    neutral: {
+      darkBlueBG: "hsl(217, 54%, 11%)",
+      darkBlueCard: "hsl(216, 50%, 16%)",
+      darkBlueLine: "hsl(215, 32%, 27%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+  }
+};
+
+export default theme;
